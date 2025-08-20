a96e2a4ca48c2f21a344c10237ce51d20d59e763
Mélanie Vandervaeren <melvdv@yahoo.fr>
2025-08-20 11:44:45 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Taches from './components/Taches.vue'
+</script>
+
+<template>
+
+  <h1>To Do List</h1>
+
+<Taches/>
+
+</template>
+
+<style scoped></style>
---
+++ src/components/Taches.vue
@@ -0,0 +1,12 @@
+<template>
+<h1>les Taches</h1>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+h1 {
+    color: red
+}
+</style>
